27078afa349f378b759ad2f4961bbe25bb00fca8
Mirco Bertelli <mircobe87@gmail.com>
2016-01-15 00:17:45 +0100

    improved overlays management
    this improvement avoids the case that multiple overlays are open
    concurrently

--- js/tiles.js
+++ js/tiles.js
@@ -1,10 +1,53 @@
 (function($) {
 
+// ==== OverlayManager =========================================================
+
+    function OverlayManager() {
+        this.list = [];
+    };
+
+    /**
+     * open the passed overlay
+     */
+    OverlayManager.prototype.open = function(overlay) {
+        var tile = overlay.parent().children('.tile');
+        var initialTop = '-' + tile.css('height');
+        overlay.css('top', 0);
+        this.list.push({
+            overlay: overlay,
+            close: function() {
+                overlay.css('top', initialTop);
+            }
+        });
+    };
+
+    /**
+     * it searches for the passed overlay in ordet to close it.
+     */
+    OverlayManager.prototype.close = function(overlay) {
+        var i = 0;
+        while (i < this.list.length && this.list[i].overlay != overlay) i++;
+        if (i < this.list.length) {
+            this.list[i].close();
+            this.list.splice(i, 1);
+        }
+    };
+
+    /**
+     * close all opened overlays
+     */
+    OverlayManager.prototype.closeAll = function() {
+        while(this.list.length > 0) {
+            this.list.pop().close();
+        }
+    };
+
     /**
     * initialize the 'black dropdown' panels that are showed
     * when the mouse pointer passes over the post tiles.
     */
-    function overlaySetup() {
+    OverlayManager.prototype.init = function() {
+        var scope = this;
         // for each dropdown panels ...
         $('.tile-overlay').each(function(index, item) {
             var overlay = $(item);
@@ -26,31 +69,42 @@
             // ... make the dropdown panel visible ...
             overlay.css('display', 'block');
 
-            // ... define the mouseenter event handler to show the dropdown panel ...
-            function showOverlay() {
-                overlay.css('top', 0);
+            // ... define the mouseenter event handler to show the dropdown
+            // panel ...
+            function showOverlay(event) {
+                scope.closeAll(); // close overlays that are still open
+                scope.open(overlay); // open the current overlay
             }
             tile.off('mouseenter').on('mouseenter', showOverlay);
 
             // ... define the mouseleave event handler to hide the dropdown
             // panel ...
-            function hideOverlay() {
-                overlay.css('top', initialTop);
+            function hideOverlay(event) {
+                scope.close(overlay);
             }
             overlay.off('mouseleave').on('mouseleave', hideOverlay);
-        })
-    }
+        });
+    };
 
+    OverlayManager.prototype.refresh = function() {
+        this.closeAll();
+        this.init();
+    };
+// =============================================================================
+
+// ---- MAIN -------------------------------------------------------------------
+    var olManager = new OverlayManager();
     // when the page become ready ...
     $(function() {
         // ... all the dropdown panels are set up ...
-        overlaySetup();
+        olManager.init();
 
         // ... a resize event handler is defined to reinitialize all the
         // dropdown panels so that they can cover correctly their post tiles.
         $(window).on('resize', function() {
-            overlaySetup();
+            olManager.refresh();
         });
     });
+// -----------------------------------------------------------------------------
 
 })(jQuery);
