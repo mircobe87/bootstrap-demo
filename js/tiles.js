0260fc6dc4cf47130d8739259970e2c18f6959f8
Mirco Bertelli <mircobe87@gmail.com>
2016-01-14 19:17:31 +0100

    fixed the appearance of the overlays during resizing of the page

--- js/tiles.js
+++ js/tiles.js
@@ -1,26 +1,57 @@
 (function($) {
+
+    /**
+    * initialize the 'black dropdown' panels that are showed
+    * when the mouse pointer passes over the post tiles.
+    */
     function overlaySetup() {
+        // for each dropdown panels ...
         $('.tile-overlay').each(function(index, item) {
             var overlay = $(item);
+            // ... get its container element ...
             var tile = overlay.parent().children('.tile');
+            // ... get the exact value to place the buttom of the dropdown panel
+            // on the top of the respective post tile ...
             var initialTop = '-' + tile.css('height');
+
+            // ... hide the dropdown panel during the setting up ...
+            overlay.css('display', 'none');
+            // ... set up the style of the dropdown panel to fit the size of its
+            // post tile ...
             overlay.css({
                 top: initialTop,
                 width: tile.css('width'),
                 height: tile.css('height')
             });
-            tile.on('mouseenter', function() {
+            // ... make the dropdown panel visible ...
+            overlay.css('display', 'block');
+
+            // ... define the mouseenter handler to show the dropdown panel ...
+            function showOverlay() {
                 overlay.css('top', 0);
-            });
-            tile.on('mouseleave', function() {
+            }
+            // ... define the mouseleave event handler to hide the dropdown
+            // panel ...
+            function hideOverlay() {
                 overlay.css('top', initialTop);
-            });
+            }
+            // ... set up the mouse events for its post tile to
+            // drop up and down the panel over its post tile.
+            tile.off('mouseenter').on('mouseenter', showOverlay)
+                .off('mouseleave').on('mouseleave', hideOverlay);
         })
     }
+
+    // when the page become ready ...
     $(function() {
+        // ... all the dropdown panels are set up ...
         overlaySetup();
+
+        // ... a resize event handler is defined to reinitialize all the
+        // dropdown panels so that they can cover correctly their post tiles.
         $(window).on('resize', function() {
             overlaySetup();
         });
     });
+
 })(jQuery);
